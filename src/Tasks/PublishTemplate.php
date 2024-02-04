a8a22eb514dda7a864ee480f5e59ffd85a0edf9b
billy joel ranario <billy.ranario@ustp.edu.ph>
2024-02-04 20:11:10 +0800

    Create TASK directory and add permission

--- src/Tasks/PublishTemplate.php
+++ src/Tasks/PublishTemplate.php
@@ -7,19 +7,26 @@ use SilverStripe\Dev\BuildTask;
 class PublishTemplate extends BuildTask
 {
     protected $title = 'Publish Template Task';
-
     protected $description = 'Creates a copy for ElementHolder.ss to the project templates directory.';
 
     public function run($request)
     {
         $source = BASE_PATH . '/vendor/thehustle/silverstripe-element-container/templates/DNADesign/Elemental/Layout/ElementHolder.ss';
-        $destination = BASE_PATH . '/app/templates/DNADesign/Elemental/Layout/ElementHolder.ss';
+        $destinationDir = BASE_PATH . '/app/templates/DNADesign/Elemental/Layout/';
+        $destination = $destinationDir . 'ElementHolder.ss';
 
         if (!file_exists($destination)) {
-            copy($source, $destination);
-            echo "Template copied successfully.\n";
+            if (!is_dir($destinationDir)) {
+                mkdir($destinationDir, 0777, true);
+            }
+            if (copy($source, $destination)) {
+                echo "Template copied successfully.\n";
+            } else {
+                echo "Failed to copy template.\n";
+            }
         } else {
             echo "Template already exists. No action taken.\n";
         }
     }
 }
+
